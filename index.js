01766e75f8bcc154081d631baaaea25f98127fab
Joshua <mwalejoshua89@gmail.com>
2022-06-07 10:45:24 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Josh Mwale';
+const welcomeMessage = 'Welcome to Flatbook, ' + 'Josh Mwale' + '!'
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "JOSH MWALE" + "!"
+const upperCaseCurrentUser = 'Josh Mwale'.toUpperCase();
+const shortGreeting = 'Welcome, ' + 'JM' + '!'
