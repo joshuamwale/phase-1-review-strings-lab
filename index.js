2334920ec0b6e028497228377e78efc2d7aec9bb
Joshua <mwalejoshua89@gmail.com>
2022-06-29 13:30:05 +0300

    commit tree b31e56d9adbfe38f1d09c314a81a63dfc668a001

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const currentUser = 'Josh Mwale';
-const welcomeMessage = 'Welcome to Flatbook, ' + 'Josh Mwale' + '!'
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "JOSH MWALE" + "!"
-const upperCaseCurrentUser = 'Josh Mwale'.toUpperCase();
+const currentUser = 'Joseph Raptu';
+const welcomeMessage = 'Welcome to Flatbook, ' + 'Joseph Raptu' + '!'
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "JOSEPH RAPTU" + "!"
+const upperCaseCurrentUser = 'Joseph Raptu'.toUpperCase();
 const shortGreeting = 'Welcome, ' + 'JM' + '!'
